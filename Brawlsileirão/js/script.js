a1bca419ff6e6c1190cbf7272dfe3e896598c954
samuelsouzaleite <128092642+samuelsouzaleite@users.noreply.github.com>
2023-07-25 20:36:15 -0300

    Add files via upload

---
+++ Brawlsileirão/js/script.js
@@ -0,0 +1,40 @@
+function p1()
+{
+    window.document.getElementById("area").innerHTML = 
+    "<img class='img-d' src='img/flamengo-semfundo.png'> <h1>FLAMENGO</h1><p class='tp'>Em 1911, houve um desentendimento interno no Fluminense. Alguns jogadores falavam trocar de clube, enquanto outros até mesmo pensavam em abandonar o futebol. Foi quando Alberto Borgerth, um dos jogadores do Fluminense, fez a proposta de criar uma seção de futebol no Flamengo, onde já era remador. A ideia foi aprovada e, no dia 8 de novembro daquele ano, foi criado o Departamento de Esportes Terrestres rubro-negro.</p> "
+}
+
+function p2()
+{
+    window.document.getElementById("area").innerHTML =
+    "<img class='img-d' src='img/vasco-semfundo.png'> <h1>VASCO</h1> <p class='tp'>O lugar que o Vasco da Gama ocupa na elite do futebol brasileiro tem a marca gloriosa do time conhecido como os camisas negras que, em 1923, com uma campanha arrasadora (11 vitórias, dois empates e apenas uma derrota), conquistou o primeiro titulo de campeão carioca de sua História.</p>"
+}
+
+function p3()
+{
+    window.document.getElementById("area").innerHTML =
+    "<img class='img-d' src='img/cruzeiro-semfundo.png'> <h1>CRUZEIRO</h1> <p class='tp'>O Cruzeiro Esporte Clube nasceu através do esforço de desportistas da comunidade italiana em Belo Horizonte, com o nome de Societá Sportiva Palestra Itália, em 2 de janeiro de 1921. Após mais de 100 anos de história, o Clube se transformou em uma das maiores agremiações de futebol do mundo. Dos anos iniciais, datam os primeiros ídolos e conquistas do Palestra, como o tricampeonato estadual de 1928, 1929 e 1930, com uma equipe que contava com os lendários Ninão, Nininho, Bengala e Piorra.</p>"
+}
+function p4()
+{
+    window.document.getElementById("area").innerHTML =
+    "<img class='img-d' src='img/furacao-semfundo.png'> <h1>ATHLETICO PARANAENSE</h1> <p class='tp'>O Clube Atlético Paranaense originou-se da fusão de dois tradicionais clubes de Curitiba - o América Foot-ball Club, até então com um titulo paranaense da fusão, que acabou, com o Paraná (America - Paraná) e do Internacional Foot-ball Club, o primeiro campeão estadual, em 1916.</p>"
+}
+
+function p5()
+{
+    window.document.getElementById("area").innerHTML =
+    "<img class='img-d' src='img/botafogo-semfundo.png'> <h1>BOTAFOGO</h1> <p class='tp'>O Botafogo de Futebol e Regatas nasceu oficialmente no dia 8 de dezembro de 1942, como resultado da fusão de dois clubes com o mesmo nome: o Club de Regatas Botafogo e o Botafogo Football Club. Os dois clubes tinham suas sedes no bairro de Botafogo, na Zona Sul do Rio de Janeiro, e se uniram depois de um triste fato.</p>"
+}
+
+function p6()
+{
+    window.document.getElementById("area").innerHTML =
+    "<img class='img-d' src='img/coritiba-semfundo.png'> <h1>CORITIBA</h1> <p class='tp'>O Coritiba Foot Ball Club é da cidade de Curitiba, no Paraná, e foi fundado por alemães. É o mais antigo time do Estado e um dos principais, sendo chamado pelos torcedores de Coxa. Foi fundado em 12 de outubro de 1909. Diversos jovens se reuniam no Clube Ginástico Teuto-Brasileiro e foram apresentados ao esporte.</p>"
+}
+
+
+function retorno()
+{
+    window.document.getElementById("area").innerHTML="<img class='logo' src='img/logo.png'>"
+}
